b92f36b250a5295d4b7161d8523271ee2c0f41f6
skcbroo <skcbroo@gmail.com>
2026-02-27 13:57:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const chatRoutes = require('./routes/chatRoutes');
+// server.js (ESM)
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import chatRoutes from "./routes/chatRoutes.js";
 
-dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', chatRoutes);
+app.use("/api", chatRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
